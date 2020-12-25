d3cbc1e7c23d95ef82a479eb407e7a5e9138831a
RAJAVEL-M <68310183+RAJAVEL-M@users.noreply.github.com>
2020-12-25 21:48:03 +0530

    Add files via upload

---
+++ djangoapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Image_model(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='images/')
+    detected_img = models.ImageField(upload_to='images/',blank=True)
+    
+
+    def delete(self,*args,**kwargs):
+        self.image.delete()
+        self.detected_img.delete()
+        super().delete(*args,**kwargs)
